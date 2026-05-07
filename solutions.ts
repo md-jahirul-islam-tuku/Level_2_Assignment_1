85ae382c340c3a68ae915c6e572c6b0d4b6de952
Tuku <tukuwebian@gmail.com>
2026-05-07 09:51:25 +0300

    Problem 2 solved

--- solutions.ts
+++ solutions.ts
@@ -1,10 +1,19 @@
-// Problem 1:
+//* Problem 1 Solution:
 
 const filterEvenNumbers = (numbers: number[]): number[] =>
   numbers.filter((number) => number % 2 === 0);
 
 // Sample input:
-const arr = [1, 2, 3, 4, 5, 6];
-const result1 = filterEvenNumbers(arr);
-// Output:
-// console.log(result1); //* [2, 4, 6]
+const result1 = filterEvenNumbers([1, 2, 3, 4, 5, 6]);
+// console.log(result1);
+
+//* Problem 2 Solution:
+
+const reverseString = (str: string): string => [...str].reverse().join("");
+
+// Sample Input:
+const result2 = reverseString("typescript");
+// console.log(result2);
+
+//* Problem 3 Solution:
+
